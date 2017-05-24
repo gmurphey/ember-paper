5e0a8ad9ddbd93ccbaefd4df973d0e7d15ccd0ab
Garrett Murphey <gmurphey@trovedata.com>
2017-05-24 12:45:55 -0400

    WIP

--- addon/components/paper-calendar.js
+++
@@ -1,8 +0,0 @@
-import PowerCalendar from 'ember-power-calendar/components/power-calendar';
-
-export default PowerCalendar.extend({
-  classNames: ['paper-calendar'],
-
-  navComponent: 'paper-calendar-nav',
-  daysComponent: 'paper-calendar-days'
-});
---
+++ addon/components/paper-date-input.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+import layout from '../templates/components/paper-date-input';
+
+const {
+  Component,
+  computed,
+  get
+} = Ember;
+
+export default Component.extend({
+  layout,
+
+  date: computed.alias('value'),
+  label: 'Date',
+  format: 'L',
+
+  actions: {
+    selectDate({ moment }) {
+      let date = get(this, 'date');
+
+      if (date) {
+        date = date.clone();
+
+        date.set({
+          month: moment.month(),
+          date: moment.date(),
+          year: moment.year()
+        });
+      } else {
+        date = moment;
+      }
+
+      get(this, 'onChange')(date);
+    }
+  }
+});
