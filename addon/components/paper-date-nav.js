1b184e2fa6d27fb27f1d85b3933fd642ab46fb0a
Garrett Murphey <gmurphey@trovedata.com>
2017-05-24 12:45:55 -0400

    WIP

--- addon/components/paper-date-nav.js
+++ addon/components/paper-date-nav.js
@@ -1,5 +1,5 @@
 import PowerCalendarNav from 'ember-power-calendar/components/power-calendar/nav';
-import layout from '../templates/components/paper-calendar-nav';
+import layout from '../templates/components/paper-date-nav';
 
 export default PowerCalendarNav.extend({
   layout,
