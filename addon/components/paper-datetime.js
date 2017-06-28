d8cc9c1011302380ef2ecaafad85fc9f2061efcd
Garrett Murphey <gmurphey@trovedata.com>
2017-06-28 12:42:05 -0400

    Adding switch to handle null values in paper-datetime.

--- addon/components/paper-datetime.js
+++ addon/components/paper-datetime.js
@@ -15,9 +15,19 @@ export default Component.extend({
 
   actions: {
     selectDate({ moment }) {
-      let datetime = get(this, 'datetime').clone();
+      let datetime = get(this, 'datetime');
 
-      datetime.set({ month: moment.month(), date: moment.date(), year: moment.year() });
+      if (datetime) {
+        datetime = datetime.clone();
+
+        datetime.set({
+          month: moment.month(),
+          date: moment.date(),
+          year: moment.year()
+        });
+      } else {
+        datetime = moment;
+      }
 
       get(this, 'onChange')(datetime);
     }
